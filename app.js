86e4935350caca0dbbd01d313ad469abae956bb5
kikinjeri <kikinjeri@proton.me>
2024-11-20 18:58:16 -0500

    Add files via upload
    9.27

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
+const fs = require ('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
