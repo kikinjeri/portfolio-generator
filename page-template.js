c0e42a1be83b8a9635d7e404099605ffbdac67d5
kikinjeri <kikinjeri@proton.me>
2024-11-20 20:06:08 -0500

    Add files via upload
    nmp inquirer install

--- page-template.js
+++ page-template.js
@@ -1,30 +1,19 @@
-const generatePage = (name, github)=> {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
+module.exports = (name, github) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
   </head>
-
+  
   <body>
-  <h1>${name}</h1>
-  <h2><a href="https://github.com/${github}">Github</a></h2>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
   </body>
-
-</html>
-`;
-
-
+  </html>
+  `;
 };
-
-fs.writeFile('./index.html', generatePage(name, github)), err=> {
-    if (err) throw new Error(err);
-    console.log('Portfolio complete! Check out index.html to see the output!');
-
-
-};
-
-module.exports = generatePage;
